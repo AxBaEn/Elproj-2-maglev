3d9c5a4821547c2a87d5a4162060217825feb0d8
AxBaEn <abackenocksson@gmail.com>
2022-02-12 12:24:42 +0100

    MATLAB upplagt

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,48 @@
 #include <Arduino.h>
 #include <PID_v1.h>
-#include <vl53l0x.h>
+#include <Wire.h>
+#include <VL53L0X.h>
+
+VL53L0X sensor;
 
 //Define pins
 const int outPin = 0;
 
 //Define Variables we'll be connecting to
-double Setpoint, Input, Output;
+double setpoint, input, output;
 
 //Define the aggressive and conservative Tuning Parameters
 double aggKp=4, aggKi=0.2, aggKd=1;
 double consKp=1, consKi=0.05, consKd=0.25;
 
 //Specify the links and initial tuning parameters
-PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
+PID myPID(&input, &output, &setpoint, consKp, consKi, consKd, DIRECT);
 
 void setup()
 {
   //initialize the variables we're linked to
-  Input = analogRead(0);
-  Setpoint = 100;
+  input = analogRead(0);
+  setpoint = 100;
 
   //turn the PID on
   myPID.SetMode(AUTOMATIC);
+
+  Serial.begin(9600);
+  Wire.begin();
+
+  sensor.setTimeout(500);
+  if (!sensor.init())
+  {
+    Serial.println("Failed to detect and initialize sensor!");
+    while (1) {}
+  }
 }
 
 void loop()
 {
-  Input = analogRead(0);
+  input = analogRead(0);
 
-  double gap = abs(Setpoint-Input); //distance away from setpoint
+  double Error = abs(setpoint-input); //distance away from setpoint
   /*
   if(gap<10)
   {  //we're close to setpoint, use conservative tuning parameters
@@ -43,5 +56,5 @@ void loop()
   */
 
   myPID.Compute();
-  analogWrite(3,Output);
+  analogWrite(3,output);
 }
