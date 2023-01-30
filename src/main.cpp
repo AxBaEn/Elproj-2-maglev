52d7d6c8c4014a4cd98e29bcade8685b4ed38eec
AxBaEn <abackenocksson@gmail.com>
2023-01-30 14:56:40 +0100

    Just bullshit

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,50 @@
 #include <Arduino.h>
 #include <PID_v1.h>
 #include <Wire.h>
-#include "Adafruit_VL53L0X.h"
+#include <Adafruit_VL53L0X.h>
+#include <pico/stdlib.h>
+#include <hardware/pwm.h>
+
  
 Adafruit_VL53L0X sensor = Adafruit_VL53L0X();
 
 //VL53L0X sensor;
 
-//Define pins
-const int outPin = 0;
+//define pins
+const int pwmPin = 1;
 
-//Define Variables we'll be connecting to
+//define constants
+const int pwmFreq = 100000;
+//uint?
+uint sliceNum; //PWM channel
+int pwmWrapPoint, pwmDutyCycle;
+
+//define Variables we'll be connecting to
 double setpoint, input, output;
 
-//Define the aggressive and conservative Tuning Parameters
+//define the aggressive and conservative Tuning Parameters
 double aggKp=4, aggKi=0.2, aggKd=1;
 double consKp=1, consKi=0.05, consKd=0.25;
 
-//Specify the links and initial tuning parameters
+//specify the links and initial tuning parameters
 PID myPID(&input, &output, &setpoint, consKp, consKi, consKd, DIRECT);
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//set wrap point on the 125MHz (8ns period) 65536 value PWM counter
+//wrap = T/(8*10^-9), T = 1/f
+void setupPwm(int pwmFreq){
+  gpio_set_function(pwmPin, GPIO_FUNC_PWM);
+  sliceNum = pwm_gpio_to_slice_num(pwmPin); //get PWM channel
+  pwm_set_enabled(sliceNum, true);
+  pwmWrapPoint = 10^9/(pwmFreq*8);
+  pwm_set_wrap(sliceNum, pwmWrapPoint); //set wrap point
+}
+
+//sets PWM duty cycle with 0 <= D <= 1
+void setPwmDutyCycle(int pwmDutyCycle){
+  pwm_set_chan_level(sliceNum, PWM_CHAN_A, pwmDutyCycle * pwmWrapPoint);
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -30,7 +55,12 @@ void setup() {
   delay(5000);
   digitalWrite(LED_BUILTIN, LOW);
   Serial.println("start");
-  //setupCall();
+  //pinMode(1, OUTPUT);
+  //digitalWrite(1, HIGH);
+
+  pwmDutyCycle = 0.5;
+  setupPwm(pwmFreq);
+  setPwmDutyCycle(pwmDutyCycle);
 
   //initialize the variables we're linked to
   input = analogRead(0);
@@ -58,6 +88,8 @@ void setup() {
 }
 
 void loop() {
+  delay(1000);
+  //Serial.println("loop");
   /*
   input = sensor.readRangeContinuousMillimeters();
   if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
